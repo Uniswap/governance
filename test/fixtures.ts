bdf9be5bce8b614fd384de5653d238ae315508ca
Noah Zinsmeister <noahwz@gmail.com>
2020-09-03 12:03:25 -0400

    add basic initialization fixture

--- test/fixtures.ts
+++ test/fixtures.ts
@@ -1,10 +1,12 @@
 import chai from 'chai'
-import { Contract, Wallet } from 'ethers'
+import { Contract, Wallet, providers, utils } from 'ethers'
 import { solidity, deployContract } from 'ethereum-waffle'
 
 import Uni from '../build/Uni.json'
 import Timelock from '../build/Timelock.json'
 import GovernorAlpha from '../build/GovernorAlpha.json'
+
+import { DELAY, mineBlock } from './utils'
 
 chai.use(solidity)
 
@@ -14,10 +16,37 @@ interface GovernanceFixture {
   governorAlpha: Contract
 }
 
-export async function governanceFixture([wallet]: Wallet[]): Promise<GovernanceFixture> {
-  const uni = await deployContract(wallet, Uni, [wallet.address])
-  const timelock = await deployContract(wallet, Timelock, [wallet.address, 60 * 60 * 24 * 2])
-  const governorAlpha = await deployContract(wallet, GovernorAlpha, [wallet.address, wallet.address, wallet.address])
+export async function governanceFixture(
+  [wallet]: Wallet[],
+  provider: providers.Web3Provider
+): Promise<GovernanceFixture> {
+  const guardian = wallet.address
+
+  // deploy UNI, sending the total supply to the guardian
+  const uni = await deployContract(wallet, Uni, [guardian])
+
+  // deploy timelock, controlled by the guardian
+  const timelock = await deployContract(wallet, Timelock, [guardian, DELAY])
+
+  // deploy governorAlpha
+  const governorAlpha = await deployContract(wallet, GovernorAlpha, [timelock.address, uni.address, guardian])
+
+  // nominate governorAlpha as pending admin
+  const target = timelock.address
+  const value = 0
+  const signature = 'setPendingAdmin(address)'
+  const data = utils.defaultAbiCoder.encode(['address'], [governorAlpha.address])
+  const { timestamp: now } = await provider.getBlock('latest')
+  const eta = now + DELAY
+  await timelock.queueTransaction(target, value, signature, data, eta)
+
+  await mineBlock(provider, now + DELAY)
+
+  await timelock.executeTransaction(target, value, signature, data, eta)
+
+  // accept admin
+  await governorAlpha.__acceptAdmin()
+  await governorAlpha.__abdicate()
 
   return { uni, timelock, governorAlpha }
 }
--- test/utils.ts
+++ test/utils.ts
@@ -1,5 +1,7 @@
 import { providers } from 'ethers'
 
+export const DELAY = 60 * 60 * 24 * 2
+
 export async function mineBlock(provider: providers.Web3Provider, timestamp: number): Promise<void> {
   return provider.send('evm_mine', [timestamp])
 }
